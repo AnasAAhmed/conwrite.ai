b3f7e615b6adc7b7c01fb770954318b354f7b384
AnasAhmed <hafeezroll69@gmail.com>
2025-06-25 20:07:59 +0500

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -12,7 +12,8 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    "/((?!.*\\..*|_next).*)",
+    "/((?!_next|.*\\..*|$).*)",
     "/(api|trpc)(.*)",
   ],
 };
+
